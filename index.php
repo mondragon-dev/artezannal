63bde04fe5629d1ca382a0670a4fd0f56ddfb526
Daniel Mondragon <68122137+mondragon-dev@users.noreply.github.com>
2023-09-06 12:38:16 -0600

    Vista movil

--- index.php
+++ index.php
@@ -70,6 +70,9 @@
     .shop-block-one .inner-box .image-box .category.red-bg::after {
       background: #DE6529;
     }
+    .mobile-menu .menu-backdrop {
+        background: #DE6529;
+    }
 </style>
 
 </head>
@@ -147,7 +150,7 @@
                                 </div>
                             </div>
                             <div class="price-box">
-                                <span>USD</span>
+                                <span>MXN</span>
                                 <ul class="price-list clearfix">
                                     <li><a href="index.php">MXN</a></li>
                                     <li><a href="index.php">USD</a></li>
@@ -241,23 +244,23 @@
             <div class="menu-backdrop"></div>
             <div class="close-btn"><i class="fas fa-times"></i></div>
             <nav class="menu-box">
-                <div class="nav-logo"><a href="index.html"><img src="assets/images/logo-2.png" alt="" title=""></a></div>
+                <div class="nav-logo"><a href="index.html"><img src="assets/images/logo--sin-fondo--textoblanco.png" alt="" title="" width="250"></a></div>
                 <div class="menu-outer"><!--Here Menu Will Come Automatically Via Javascript / Same Menu as in Header--></div>
                 <div class="contact-info">
-                    <h4>Contact Info</h4>
+                    <h4>Contacto</h4>
                     <ul>
-                        <li>Chicago 12, Melborne City, USA</li>
-                        <li><a href="tel:+8801682648101">+88 01682648101</a></li>
-                        <li><a href="mailto:info@example.com">info@example.com</a></li>
+                        <li>CDMX, México</li>
+                        <li><a href="tel:+5211234567890">+(521) 1234-567-890</a></li>
+                        <li><a href="mailto:info@artezannal.com">info@artezannal.com</a></li>
                     </ul>
                 </div>
                 <div class="social-links">
                     <ul class="clearfix">
-                        <li><a href="index.html"><span class="fab fa-twitter"></span></a></li>
-                        <li><a href="index.html"><span class="fab fa-facebook-square"></span></a></li>
-                        <li><a href="index.html"><span class="fab fa-pinterest-p"></span></a></li>
-                        <li><a href="index.html"><span class="fab fa-instagram"></span></a></li>
-                        <li><a href="index.html"><span class="fab fa-youtube"></span></a></li>
+                        <li><a href="index.php"><span class="fab fa-twitter"></span></a></li>
+                        <li><a href="index.php"><span class="fab fa-facebook-square"></span></a></li>
+                        <li><a href="index.php"><span class="fab fa-pinterest-p"></span></a></li>
+                        <li><a href="index.php"><span class="fab fa-instagram"></span></a></li>
+                        <li><a href="index.php"><span class="fab fa-youtube"></span></a></li>
                     </ul>
                 </div>
             </nav>
